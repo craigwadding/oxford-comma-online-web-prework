ebd0b2509f96fb330565ddbe6b7057954ac681bd
craig wadding <craig.wadding@gmail.com>
2019-04-24 00:50:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,18 @@
-def oxford_comma([kiwi]).join
-
+def oxford_comma(array)
+  if array.length == 1 
+  array.join('')
   
-
-end
+  elsif array.length == 2 
+    array.join(' and ')
+    elsif array.length >= 3 
+      array[-1] = "and #{array[-1]}"
+      array.join(', ')
+    else 
+      nil 
+    end 
+end   
+  
+oxford_comma(["kiwi"])
+oxford_comma(["kiwi", "durian"])
+oxford_comma(["kiwi", "durian", "starfruit"])
+oxford_comma(['kiwi', 'durian', 'starfruit', 'dragon fruits', 'mangos'])
